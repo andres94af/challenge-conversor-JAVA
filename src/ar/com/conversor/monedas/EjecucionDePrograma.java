a64764e0f2b84faed967002f71ba091956746da0
Andres Fernandez <andres94.af@gmail.com>
2022-08-24 16:04:24 +0200

    programa funcionando metodo pesos a dolar

--- src/ar/com/conversor/monedas/EjecucionDePrograma.java
+++ src/ar/com/conversor/monedas/EjecucionDePrograma.java
@@ -1,5 +1,33 @@
 package ar.com.conversor.monedas;
 
+import javax.swing.*;
+
 public class EjecucionDePrograma {
+	
+	public static void main(String[] args) {
+		
+		TipoDeConversion conversion = new TipoDeConversion();
+		
+		while(true){
+			String opciones = JOptionPane.showInputDialog(null, "Seleccione que desea convertir", "Menu", JOptionPane.PLAIN_MESSAGE, null,
+					new Object[] {"Convertir monedas", "Convertir medidas"},"Seleccionar").toString();
+			
+			switch (opciones) {
+			case "Convertir monedas":
+				double numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el monto a convertir"));
+				conversion.ConvertirMoneda(numero);
+				int rta = JOptionPane.showConfirmDialog(null, "¿Desea realizar otra conversion?", "OPCIONES si o no", JOptionPane.YES_NO_OPTION);
+				if(JOptionPane.OK_OPTION == rta)
+				{}else {
+					JOptionPane.showMessageDialog(null,"El programa finalizo!", "Finalizar", JOptionPane.INFORMATION_MESSAGE);
+					return;
+				}
+				break;
+//			case "Convertir medidas":
+//				System.out.println("selecciono medidas");
+//				break;
+			}
+		}
+	}
 
 }
--- src/ar/com/conversor/monedas/OpcionesDeConversion.java
+++ src/ar/com/conversor/monedas/OpcionesDeConversion.java
@@ -1,5 +1,12 @@
 package ar.com.conversor.monedas;
 
+import javax.swing.JOptionPane;
+
 public class OpcionesDeConversion {
 
+	public void pARaDolar(double valor) {
+		double cotizacion = valor * 0.00730499;
+		cotizacion = (double)Math.round(cotizacion *100d)/100;
+		JOptionPane.showMessageDialog(null,"$"+valor+" equivalen a: U$D"+cotizacion, "Valor convertido", JOptionPane.INFORMATION_MESSAGE);
+	}
 }
