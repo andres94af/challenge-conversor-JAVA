839c5165b090f55be26d6ee191841333ff505ca2
Andres Fernandez <andres94.af@gmail.com>
2022-08-24 20:53:02 +0200

    agregando manejo de error. solo numeros

--- src/ar/com/conversor/ejecutable/EjecucionDePrograma.java
+++ src/ar/com/conversor/ejecutable/EjecucionDePrograma.java
@@ -15,18 +15,24 @@ public class EjecucionDePrograma {
 		while(respuesta){
 			String opciones = JOptionPane.showInputDialog(null, "Seleccione que desea convertir", "Menu", JOptionPane.PLAIN_MESSAGE, null,
 					new Object[] {"Convertir monedas", "Convertir medidas"},"Seleccionar").toString();
-			double valor = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor a convertir"));
+			double valor = 0;
 			
-			switch (opciones) {
-			case "Convertir monedas":
-				OpcionesDeConversionMoneda convMoneda = new OpcionesDeConversionMoneda();
-				convMoneda.ConvertirMoneda(valor);
-				break;
-			case "Convertir medidas":
-				OpcionesDeConversionMedida convMedida = new OpcionesDeConversionMedida();
-				convMedida.ConvertirMedida(valor);
-				break;
+			try {
+				valor = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor a convertir"));
+				switch (opciones) {
+				case "Convertir monedas":
+					OpcionesDeConversionMoneda convMoneda = new OpcionesDeConversionMoneda();
+					convMoneda.ConvertirMoneda(valor);
+					break;
+				case "Convertir medidas":
+					OpcionesDeConversionMedida convMedida = new OpcionesDeConversionMedida();
+					convMedida.ConvertirMedida(valor);
+					break;
+				}
+			}catch(NumberFormatException err) {
+				JOptionPane.showMessageDialog(null, "Solo puede ingresar numeros","Error.", JOptionPane.ERROR_MESSAGE);
 			}
+			
 			respuesta = rta.rtaFin();
 		}
 	}
--- src/ar/com/conversor/ejecutable/Respuesta.java
+++ src/ar/com/conversor/ejecutable/Respuesta.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class Respuesta {
 	
 	public boolean rtaFin() {
-		int rta = JOptionPane.showConfirmDialog(null, "¿Desea realizar otra conversion?", "OPCIONES si o no", JOptionPane.YES_NO_OPTION);
+		int rta = JOptionPane.showConfirmDialog(null, "¿Desea realizar otra conversion?", "Seleccione SI o NO", JOptionPane.YES_NO_OPTION);
 		if(JOptionPane.OK_OPTION == rta){
 			return true;
 		}else {
