65080b1fa53d4077e300099fa78446bf0f810d2d
Andres Fernandez <andres94.af@gmail.com>
2022-08-25 15:08:26 +0200

    modificando textos

--- src/ar/com/conversor/medidas/ConversionMedida.java
+++ src/ar/com/conversor/medidas/ConversionMedida.java
@@ -7,24 +7,24 @@ public class ConversionMedida {
 	public void metroACentimetro(double valor) {
 		double medida = valor * 100;
 		medida = (double)Math.round(medida *100d)/100;
-		JOptionPane.showMessageDialog(null,valor+" m equivalen a: "+ medida+" cm.","valor", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,valor+" m equivalen a: "+ medida+" cm","Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 	public void metroAKilometro(double valor) {
 		double medida = valor / 1000;
 		medida = (double)Math.round(medida *100d)/100;
-		JOptionPane.showMessageDialog(null,valor+" m equivalen a: "+ medida+" Km.","valor", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,valor+" m equivalen a: "+ medida+" Km","Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 	public void kilometroAMilla(double valor) {
 		double medida = valor * 1609;
 		medida = (double)Math.round(medida *100d)/100;
-		JOptionPane.showMessageDialog(null,valor+" km equivalen a: "+ medida+ " milla.","valor", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,valor+" km equivalen a: "+ medida+ " milla","Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 	public void metroAPies(double valor) {
 		double medida = valor * 3.28084;
 		medida = (double)Math.round(medida *100d)/100;
-		JOptionPane.showMessageDialog(null,valor+" m equivale a: "+ medida+ " pies.","valor", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,valor+" m equivalen a: "+ medida+ " pies","Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}
 }
--- src/ar/com/conversor/monedas/ConversionMoneda.java
+++ src/ar/com/conversor/monedas/ConversionMoneda.java
@@ -7,60 +7,60 @@ public class ConversionMoneda {
 	public void pARaDolar(double valor) {
 		double cotizacion = valor * 0.00730499;
 		cotizacion = (double)Math.round(cotizacion *100d)/100;
-		JOptionPane.showMessageDialog(null,"$"+valor+" equivalen a: U$D"+cotizacion, "Valor convertido", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,"$"+valor+" equivalen a: U$D"+cotizacion, "Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}//ok
 	
 	public void pARaEuro(double valor) {
 		double cotizacion = valor * 0.0073590735;
 		cotizacion = (double)Math.round(cotizacion *100d)/100;
-		JOptionPane.showMessageDialog(null,"$"+valor+" equivalen a: €"+cotizacion, "Valor convertido", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,"$"+valor+" equivalen a: €"+cotizacion, "Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}//ok
 	
 	public void pARaLibra(double valor) {
 		double cotizacion = valor * 0.0062016523;
 		cotizacion = (double)Math.round(cotizacion *100d)/100;
-		JOptionPane.showMessageDialog(null,"$"+valor+" equivalen a: £"+cotizacion, "Valor convertido", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,"$"+valor+" equivalen a: £"+cotizacion, "Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}//ok
 	
 	public void pARaYen(double valor) {
 		double cotizacion = valor * 1.0002996 ;
 		cotizacion = (double)Math.round(cotizacion *100d)/100;
-		JOptionPane.showMessageDialog(null,"$"+valor+" equivalen a: ¥"+cotizacion, "Valor convertido", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,"$"+valor+" equivalen a: ¥"+cotizacion, "Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}//ok
 	
 	public void pARaWon(double valor) {
 		double cotizacion = valor * 9.788061;
 		cotizacion = (double)Math.round(cotizacion *100d)/100;
-		JOptionPane.showMessageDialog(null,"$"+valor+" equivalen a: ₩"+cotizacion, "Valor convertido", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,"$"+valor+" equivalen a: ₩"+cotizacion, "Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}//ok
 	
 	public void dolarAPAr(double valor) {
 		double cotizacion = valor * 137.15335;
 		cotizacion = (double)Math.round(cotizacion *100d)/100;
-		JOptionPane.showMessageDialog(null,"U$D"+valor+" equivalen a: $"+cotizacion, "Valor convertido", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,"U$D"+valor+" equivalen a: $"+cotizacion, "Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}//ok
 	
 	public void euroAPAr(double valor) {
 		double cotizacion = valor * 136.04444;
 		cotizacion = (double)Math.round(cotizacion *100d)/100;
-		JOptionPane.showMessageDialog(null,"€"+valor+" equivalen a: $"+cotizacion, "Valor convertido", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,"€"+valor+" equivalen a: $"+cotizacion, "Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}//ok
 	
 	public void libraAPAr(double valor) {
 		double cotizacion = valor * 161.42513;
 		cotizacion = (double)Math.round(cotizacion *100d)/100;
-		JOptionPane.showMessageDialog(null,"£"+valor+" equivalen a: $"+cotizacion, "Valor convertido", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,"£"+valor+" equivalen a: $"+cotizacion, "Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}//ok
 	
 	public void yenAPAr(double valor) {
 		double cotizacion = valor * 1.0003142;
 		cotizacion = (double)Math.round(cotizacion *100d)/100;
-		JOptionPane.showMessageDialog(null,"¥"+valor+" equivalen a: $"+cotizacion, "Valor convertido", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,"¥"+valor+" equivalen a: $"+cotizacion, "Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}//ok
 	
 	public void wonAPAr(double valor) {
 		double cotizacion = valor * 0.10217064;
 		cotizacion = (double)Math.round(cotizacion *100d)/100;
-		JOptionPane.showMessageDialog(null,"₩"+valor+" equivalen a: $"+cotizacion, "Valor convertido", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null,"₩"+valor+" equivalen a: $"+cotizacion, "Valor obtenido", JOptionPane.INFORMATION_MESSAGE);
 	}//ok
 }
